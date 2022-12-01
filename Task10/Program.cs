e6e32aaafe9e231c7230012827dfb9c6678db66c
DenisovPavel <Waste11@mail.ru>
2022-12-01 13:27:33 +0300

    Task23 done

---
+++ Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// Напишите программу, которая принимает на вход 2 числа (N,M) 
+// и выдаёт таблицу кубов чисел от N до M.
+
+// 2,3 -> 8, 27
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (numberN > numberM)
+{
+    Console.WriteLine($"Ошибка ввода чисел! Число {numberN} не должно быть больше числа {numberM}! Повторите ввод!");
+}
+while (numberN < numberM & count <= numberM)
+{
+    int vkube = numberN * numberN * numberN;
+    Console.WriteLine($"Число {numberN} в кубической степени - {vkube}");
+    numberN++;
+    count++;
+}
+
