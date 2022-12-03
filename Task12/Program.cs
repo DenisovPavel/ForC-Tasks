5dd972d79e7f70885aa75082d9f896c3ddb317a5
DenisovPavel <Waste11@mail.ru>
2022-12-03 23:08:14 +0300

    task12 done

---
+++ Task12/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0)
+{
+    Console.Write("Введите корректное число! Ваше число ровняется нулю или имеет отрицательное значение! ");
+}
+
+else
+{
+    Console.Write($"Сумма цифр в числе {number} ровна : {getSumNumbers(number)}");
+}
+
+int getSumNumbers(int num)
+{
+    int last = 0;
+
+    while (num > 0)
+    {
+        last = last + (num % 10); //last number
+        num = num / 10; // сокращаем число для подбора след.last number
+    }
+    return last;
+}
